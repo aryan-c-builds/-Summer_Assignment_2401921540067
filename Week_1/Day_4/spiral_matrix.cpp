ef879c3051d8ddf1d2492ff4c6e70d6095f1a3a8
Aryan chaturvedi <chaturvediaryan2265@gmail.com>
2026-06-04 23:38:05 +0530

    spiral_matrix

---
+++ Week_1/Day_4/spiral_matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int srow = 0, scol = 0;
+        int erow = matrix.size() - 1, ecol = matrix[0].size() - 1;
+
+        while(srow<=erow && scol<=ecol) {
+            //top
+            for (int j = scol; j <= ecol; j++){
+                ans.push_back(matrix[srow][j]);
+            }
+            
+            //right
+            for (int i = srow + 1; i <= erow; i++){
+                ans.push_back(matrix[i][ecol]);
+            }
+            
+            //bottom
+            for (int j = ecol - 1; j >= scol; j--){
+                if (srow == erow){
+                    break;
+                } 
+                ans.push_back(matrix[erow][j]);
+            }
+            
+            //left
+            for (int i = erow - 1; i >= srow + 1; i--){
+                if (scol == ecol){
+                    break;
+                } 
+                ans.push_back(matrix[i][scol]);
+            }
+
+            srow++;
+            scol++;
+            erow--;
+            ecol--;
+        }
+        return ans;
+    }
+};
